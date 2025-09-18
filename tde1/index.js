2fca9c6ce9eb464f1d9396f97f62400249db7bbf
Pedro-hotz <168838577+Pedro-hotz@users.noreply.github.com>
2025-09-17 23:57:41 -0300

    Create tde1

---
+++ tde1/index.js
@@ -0,0 +1,72 @@
+// 1) --------------------------------------------------------
+
+// let idade = Number(prompt("Digite sua idade:  "));
+
+// if (idade < 18 ) {
+//     console.log("Você ainda não pode votar");
+// } else if ( idade >= 70 ) {
+//     console.log("Você não precisa mais votar");
+// } else {
+//     console.log("Você precisa votar"); 
+// }
+
+
+// 2) ---------------------------------------------------------
+
+// let num1 = Number(prompt("Digite o primeiro número: ")); 
+// let num2 = Number(prompt("Digite o segundo número: ")); 
+
+
+// let escolha = Number(prompt(`
+//     === ESCOLHA =====
+//     | [ 1 ] Soma              
+//     | [ 2 ] Subtração         
+//     | [ 3 ] Multiplicação     
+//     | [ 4 ] Divisão           
+//     =============== 
+//     ` + "Digite o número da operação que você quer realizar entre " + num1 + " e " + num2));
+
+
+// switch(escolha){
+//     case 1: // Soma 
+//     console.log("Você escolheu SOMA");
+//     console.log("A soma de " + num1 + " + " + num2 + " = " + (num1 + num2));
+//     break; 
+
+//     case 2: // Subtração 
+//     console.log("Você escolheu SUBTRAÇÃO");
+//     console.log("A subtração de " + num1 + " - " + num2 + " = " + (num1 - num2));
+//     break; 
+
+//     case 3: // Multiplicação 
+//     console.log("Você escolheu MULTIPLICAÇÃO");
+//     console.log("A multiplicação de " + num1 + " X " + num2 + " = " + (num1 * num2));
+//     break; 
+
+//     case 4: // Divisão 
+//     console.log("Você escolheu DIVISÃO");
+//     console.log("A divisão de " + num1 + " / " + num2 + " = " + (num1 / num2) );
+//     break; 
+// }
+
+
+// 3) ---------------------------------------------------------
+
+// let sal = Number(prompt("Digite seu salário: ")); 
+
+// if (sal <= 280) {
+//     console.log("Seu novo salário é " + (sal * 1.2));
+// } else if (sal > 280 || sal < 700) {
+//     console.log("Seu novo salário é " + (sal * 1.15));
+// } else if (sal > 700 || sal < 1500) {
+//     console.log("Seu novo salário é " + (sal * 1.1));
+// }else if (sal < 1500) {
+//     console.log("Seu novo salário é " + (sal * 1.05));
+// }
+
+
+// 4) ---------------------------------------------------------
+
+
+
+
